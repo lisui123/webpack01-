18d2ab607f2e812d9831b35c11af4aa5241e7691
lisui <979387030@qq.com>
2022-06-17 20:54:16 +0800

    自动生成html文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const {join} =require('path')
-
+const HTMLWebpackPlugin=require('html-webpack-plugin')
 module.exports = {
     entry:'./src/main.js',
     output:{
         path:join(__dirname, 'lib'),
         filename:"webpack-demo.js",
         clean:true
-    }
+    },
+    plugins:[
+        new HTMLWebpackPlugin ({
+            template:join(__dirname, 'public/index.html'),
+        })
+    ]
 }
