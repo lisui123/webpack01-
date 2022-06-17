ec7dd884337288b3aaf4362ae4873b22f9280038
lisui <979387030@qq.com>
2022-06-17 21:15:48 +0800

    webpack开发服务器

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,22 @@ const {join} =require('path')
 const HTMLWebpackPlugin=require('html-webpack-plugin')
 module.exports = {
     entry:'./src/main.js',
+    mode:'development',
     output:{
         path:join(__dirname, 'lib'),
-        filename:"webpack-demo.js",
-        clean:true
+        filename: "main.js" // 出口文件名
     },
     plugins:[
         new HTMLWebpackPlugin ({
             template:join(__dirname, 'public/index.html'),
+            filename: "webpack-demo.js",
+
+        clean: true
+
         })
-    ]
+    ],
+    devServer:{
+        open:true,
+        port:65527
+    }
 }
