afc30cdae60dbe35ec5b9fdcc16aec07b120adb7
lisui <979387030@qq.com>
2022-06-17 21:38:16 +0800

    加载器 - 处理css文件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {marquee} from './marquee.js'
 import {tab } from './tab.js'
-
+import './style/index.css'
 tab()
 marquee()
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
-const {join} =require('path')
-const HTMLWebpackPlugin=require('html-webpack-plugin')
+const { join } = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    entry:'./src/main.js',
-    mode:'development',
-    output:{
-        path:join(__dirname, 'lib'),
-        filename: "main.js" // 出口文件名
-    },
-    plugins:[
-        new HTMLWebpackPlugin ({
-            template:join(__dirname, 'public/index.html'),
-            filename: "webpack-demo.js",
-
+    entry: './src/main.js',
+    mode: 'development',
+    output: {
+        path: join(__dirname, 'lib'),
+        filename: "webpack-demo.js",
         clean: true
-
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: join(__dirname, 'public/index.html'),
         })
     ],
-    devServer:{
-        open:true,
-        port:65527
+    devServer: {
+        open: true,
+        port: 65527
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/i,
+                use: ["style-loader", "css-loader"],
+            }
+        ]
     }
 }
